da4f591d21cac631bf86511358f051e20fd1fb63
tkr <plumium@outlook.com>
2024-11-13 16:37:04 +0900

    Move csv record loop into a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use std::io;
+extern crate csv;
+
+use std::{io, process};
 
 fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+// Returns a Boxed error trait, which makes it harder for callers
+// to match on specific error types or handle different error conditions
+// in a more granular way.
+fn run() -> Result<(), Box<dyn std::error::Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
     for result in rdr.records() {
-        let record = result.expect("a CSV record");
+        let record = result?;
         println!("{:?}", record);
     }
+
+    Ok(())
 }
