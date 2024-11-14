849ac83009bb9204ac0c9b49237cde6a4803487e
tkr <plumium@outlook.com>
2024-11-15 04:21:48 +0900

    update: read records deal with Serde deserialization

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,24 @@ extern crate csv;
 
 use std::{env, ffi::OsString, process};
 
+use serde::Deserialize;
+
 type Error = Box<dyn std::error::Error>;
+
+// We don't need to derive `Debug` (which doesn't require Serde), but it's a
+// good habit to do it for all your types.
+//
+// Notice that the field names in this struct are NOT in the same order as
+// the fields in the CSV data!
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Record {
+    latitude: f64,
+    longitude: f64,
+    population: Option<u64>,
+    city: String,
+    state: String,
+}
 
 fn main() {
     if let Err(err) = run() {
@@ -17,9 +34,11 @@ fn main() {
 fn run() -> Result<(), Error> {
     let file_path = get_first_arg()?;
     let mut rdr = csv::Reader::from_path(file_path)?;
-    for result in rdr.records() {
-        let record = result?;
+    for result in rdr.deserialize() {
+        let record: Record = result?;
         println!("{:?}", record);
+        // Try this if you don't like each record smushed on one line:
+        // println!("{:#?}", record);
     }
 
     Ok(())
