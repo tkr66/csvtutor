75353a4341ce507a39c9b5c18efd3f26eaa4aed9
tkr <plumium@outlook.com>
2024-11-15 03:07:25 +0900

    Make our program to accept a file path as an argument instead of stdin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate csv;
 
-use std::{io, process};
+use std::{env, ffi::OsString, process};
 
 fn main() {
     if let Err(err) = run() {
@@ -13,11 +13,19 @@ fn main() {
 // to match on specific error types or handle different error conditions
 // in a more granular way.
 fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
+    let file_path = get_first_arg()?;
+    let mut rdr = csv::Reader::from_path(file_path)?;
     for result in rdr.records() {
         let record = result?;
         println!("{:?}", record);
     }
 
     Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn std::error::Error>> {
+    match env::args_os().nth(1) {
+        Some(arg1) => Ok(arg1),
+        None => Err(From::from("expected 1 argument, but got none")),
+    }
 }
